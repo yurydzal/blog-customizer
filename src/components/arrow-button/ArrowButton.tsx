987ba63ee222874930e8e73ab0f4c1ae3deb2f72
Yury Dzal <yurydzal@yandex.ru>
2024-06-28 07:38:37 +0400

    Revert "review"
    This reverts commit 788866937613d9c8f9af62b99330dab76ac2be80.

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -5,25 +5,15 @@ import styles from './ArrowButton.module.scss';
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-type Props = {
-	openStatus?: () => void;
-	open?: boolean;
-};
-
-export const ArrowButton = ({ openStatus, open }: Props) => {
+export const ArrowButton = () => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container + (open ? ` ${styles.container_open}` : '')}
-			onClick={openStatus}>
-			<img
-				src={arrow}
-				alt='иконка стрелочки'
-				className={styles.arrow + (open ? ` ${styles.arrow_open}` : '')}
-			/>
+			className={styles.container}>
+			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
 		</div>
 	);
 };
